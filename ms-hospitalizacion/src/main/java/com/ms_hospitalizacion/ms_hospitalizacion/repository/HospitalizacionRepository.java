77aaf159a7890218d9176632bcc093bf6ec1eef9
macaleigh <ma.leighton@duocuc.cl>
2026-07-18 00:08:45 -0400

    Version final HospitalCare Microservices: 10 microservicios, Eureka, API Gateway, tests unitarios y relacion JPA

--- ms-hospitalizacion/src/main/java/com/ms_hospitalizacion/ms_hospitalizacion/repository/HospitalizacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ms_hospitalizacion.ms_hospitalizacion.repository;
-
-import com.ms_hospitalizacion.ms_hospitalizacion.model.Hospitalizacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HospitalizacionRepository extends JpaRepository<Hospitalizacion, Integer> {
-}
--- ms-hospitalizacion/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE hospitalizacion (
-                                 id INT AUTO_INCREMENT PRIMARY KEY,
-                                 paciente_id INT NOT NULL,
-                                 habitacion VARCHAR(50) NOT NULL,
-                                 fecha_ingreso DATE NOT NULL,
-                                 fecha_alta DATE,
-                                 motivo VARCHAR(255) NOT NULL
-);
